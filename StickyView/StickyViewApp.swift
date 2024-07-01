49901adb55aef52a878c4000d76ea240147ded6b
Nick Kibysh <nick.kibysh@nordicsemi.no>
2024-07-01 11:30:58 +0200

    Initial Commit

---
+++ StickyView/StickyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyViewApp.swift
+//  StickyView
+//
+//  Created by Nick Kibysh on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct StickyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
